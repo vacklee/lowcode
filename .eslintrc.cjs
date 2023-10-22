9ba507fc5b874004e509075d69d554fc5fa1c128
leexiaohui <1287338537>
2023-10-22 17:14:43 +0800

    init: 框架初始化

---
+++ .eslintrc.cjs
@@ -0,0 +1,58 @@
+const prettierOptions = {
+  semi: false,
+  singleQuote: true,
+  printWidth: 80,
+  trailingComma: 'none',
+  arrowParens: 'avoid'
+}
+
+module.exports = {
+  overrides: [
+    {
+      files: ['**/*.js', '**/*.cjs'],
+      extends: ['plugin:prettier/recommended'],
+      env: {
+        es2021: true,
+        node: true
+      },
+      parserOptions: {
+        sourceType: 'module',
+        ecmaVersion: 6
+      },
+      rules: {
+        'prettier/prettier': ['error', prettierOptions]
+      }
+    },
+    {
+      files: ['**/*.ts', '**/*.tsx'],
+      extends: [
+        'plugin:prettier/recommended',
+        'plugin:@typescript-eslint/recommended'
+      ],
+      parser: '@typescript-eslint/parser',
+      parserOptions: {
+        parser: '@typescript-eslint/parser',
+        sourceType: 'module',
+        ecmaFeatures: {
+          jsx: true
+        }
+      },
+      plugins: ['@typescript-eslint'],
+      rules: {
+        'prettier/prettier': ['error', prettierOptions]
+      }
+    },
+    {
+      files: ['**/*.vue'],
+      extends: ['plugin:prettier/recommended', 'plugin:vue/vue3-recommended'],
+      parser: 'vue-eslint-parser',
+      parserOptions: {
+        parser: '@typescript-eslint/parser',
+        sourceType: 'module'
+      },
+      rules: {
+        'prettier/prettier': ['error', prettierOptions]
+      }
+    }
+  ]
+}
